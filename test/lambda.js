9748bfdf1c63764423bb2f0ed6d2c5d746a0b91e
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-07-04 17:15:09 +0530

    add already existing cognito

--- test/lambda.js
+++ test/lambda.js
@@ -2,15 +2,14 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
-
     cognito_idp.adminCreateUser({
-        UserPoolId: process.env.UserPoolId_cognitoaaa, /* required */
+        UserPoolId: "us-east-1_uVXTQInep", /* required */
         Username: "andun@adroitlogic.com", /* required */
         DesiredDeliveryMediums: ["SMS"],//[SMS | EMAIL,/* more items */],
         ForceAliasCreation: false,
         MessageAction: "RESEND",
         TemporaryPassword: "Andun!12345",
-        UserAttributes: [],
+        UserAttributes: [{ Name: 'name', Value: 'Andun' }, { Name: 'email', Value: 'andun@adroitlogic.com' }, { Name: 'birthdate', Value: '02.01.1993' }, { Name: 'phone_number', Value: '+94770630943' }, { Name: 'address', Value: 'Cmb' }],
         ValidationData: []
     }, function (error, data) {
         if (error) {
