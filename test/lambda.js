a6c0092d3a659a5bb7735dd8c89ab775b94ec088
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-07-04 17:12:07 +0530

    create a cognito user pool

--- test/lambda.js
+++ test/lambda.js
@@ -1,6 +1,25 @@
 let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
-exports.handler = function(event, context, callback) {
-    
-    callback(null,'Successfully executed!');
+exports.handler = function (event, context, callback) {
+
+    cognito_idp.adminCreateUser({
+        UserPoolId: process.env.UserPoolId_cognitoaaa, /* required */
+        Username: "andun@adroitlogic.com", /* required */
+        DesiredDeliveryMediums: ["SMS"],//[SMS | EMAIL,/* more items */],
+        ForceAliasCreation: false,
+        MessageAction: "RESEND",
+        TemporaryPassword: "Andun!12345",
+        UserAttributes: [],
+        ValidationData: []
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+
+    callback(null, 'Successfully executed');
 }
