7625841f86d3ad98f4c9420aa201e6c63210d15c
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-08-24 12:03:03 +0530

    aaaa

--- test/lambda.js
+++ test/lambda.js
@@ -1,24 +1,6 @@
 let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-
-exports.handler = function (event, context, callback) {
-    cognito_idp.adminCreateUser({
-        UserPoolId: "us-east-1_uVXTQInep", /* required */
-        Username: "andun@adroitlogic.com", /* required */
-        DesiredDeliveryMediums: ["SMS"],//[SMS | EMAIL,/* more items */],
-        ForceAliasCreation: false,
-        MessageAction: "RESEND",
-        TemporaryPassword: "Andun!12345",
-        UserAttributes: [{ Name: 'name', Value: 'Andun' }, { Name: 'email', Value: 'andun@adroitlogic.com' }, { Name: 'birthdate', Value: '02.01.1993' }, { Name: 'phone_number', Value: '+94770630943' }, { Name: 'address', Value: 'Cmb' }],
-        ValidationData: []
-    }, function (error, data) {
-        if (error) {
-            // implement error handling logic here
-            throw error;
-        }
-        // your logic goes within this block
-    });
+exports.handler = function(event, context, callback) {
 
 
-    callback(null, 'Successfully executed');
+	callback(null,'Successfully executed');
 }
