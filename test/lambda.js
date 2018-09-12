645b11fda8a09ae94777069e10c43d4ecd734160
TestAndunR <andunranmal@gmail.com>
2018-09-12 11:45:53 +0530

    add a ddb remove cw event

--- test/lambda.js
+++ test/lambda.js
@@ -1,6 +1,15 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
+    ddb.put({
+        TableName: 'smokeDDB',
+        Item: { 'testId': '001', 'testType': 'smoke', 'testItem': 'DDb' }
+    }).promise().then(function (data) {
+        //your logic goes here
+    }).catch(function (err) {
+        //handle error
+    });
     console.log("Test")
-    callback(null,'Successfully executed');
+    callback(null, 'Successfully executed');
 }
