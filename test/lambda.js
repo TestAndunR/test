de86b8ad68597b0563090979350956f96bd0f742
TestAndunR <andunranmal@gmail.com>
2019-01-24 10:07:42 +0530

    ass

--- test/lambda.js
+++ test/lambda.js
@@ -1,7 +1,4 @@
-let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-
-exports.handler = function (event, context, callback) {
-    console.log("Hell world")
-    callback(null, { "message": "Successfully executed" });
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
