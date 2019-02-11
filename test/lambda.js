faa74f87ba6a942eb6deb413a7d726a8d1d2d484
TestAndunR <andunranmal@gmail.com>
2019-02-11 12:19:39 +0530

    hhh

--- test/lambda.js
+++ test/lambda.js
@@ -1,14 +1,4 @@
-let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-
-exports.handler = function (event, context, callback) {
-    ddb.scan({
-        TableName: 'HS_Redeem'
-    }).promise().then(function (data) {
-        //your logic goes here
-    }).catch(function (err) {
-        //handle error
-    });
-
-    callback(null, { "message": "Successfully executed" });
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
